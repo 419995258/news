6c0b45ca486936ae637e9094d6cbb23bb2ed61c2
pb <419995258@qq.com>
2018-11-02 15:30:40 +0800

    添加一些文件

---
+++ src/main/resources/web/base/admin/angular/place.js
@@ -0,0 +1,8 @@
+var homeAppmodule = angular.module('placeApp', []);
+
+homeAppmodule.controller('placeController', function($scope, $http, $rootScope, $stateParams, $state) {
+	
+
+
+	
+});
---
+++ src/main/resources/web/base/admin/angular/sql.js
@@ -0,0 +1,8 @@
+var homeAppmodule = angular.module('sqlApp', []);
+
+homeAppmodule.controller('sqlController', function($scope, $http, $rootScope, $stateParams, $state) {
+
+
+    $scope.data = [{'sqlKey':'a','name':'http://www.taobao.com/'},{'sqlKey':'京东','name':'http://www.jd.com/'},{'sqlKey':'唯品会','name':'http://www.vip.com/'},{'sqlKey':'百度','name':'http://www.baidu.com/'}];
+	
+});
